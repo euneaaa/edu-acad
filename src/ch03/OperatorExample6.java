496c7c5a00a64f6eeb77f4d14d89308c8ed42e27
Administrator <lznil656@gmail.com>
2021-09-30 09:51:09 +0900

    first

--- src/ch03/OperatorExample6.java
+++ src/ch03/OperatorExample6.java
@@ -4,7 +4,7 @@ public class OperatorExample6 {
     public static void main(String[] args){
         int n1 = 10;
         int n2 = 0;
-
+        //나누기에 0을 넣으면 안된다.
         System.out.println(n1 / n2);
     }
 }
